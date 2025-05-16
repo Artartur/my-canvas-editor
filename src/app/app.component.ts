42ae7a6e6c523af567a3f8a1ec207138eb66fb59
artartur <artur.souza@tds.company>
2025-05-16 01:08:24 -0300

    feat: add elements in the whiteboard like rectangles and stars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,75 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent { }
+export class AppComponent {
+  @ViewChild('canvasControl', { static: true }) canvasControl: ElementRef;
+
+  public maxElements: number = 2;
+  public elementsCounter: number = 0;
+
+  constructor(private renderer: Renderer2) { }
+
+  private generateRandomPosition(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
+
+  public clearCanvas() {
+    let content = this.canvasControl.nativeElement
+    while (content.firstChild) {
+      this.renderer.removeChild(content, content.firstChild)
+    }
+
+    this.elementsCounter = 0;
+  }
+
+  public createRectangle() {
+    if (this.elementsCounter >= this.maxElements) {
+      return;
+    }
+
+    let content = this.canvasControl.nativeElement.querySelector('svg');
+
+    if (!content) {
+      content = this.renderer.createElement('svg', 'svg');
+      this.renderer.setAttribute(content, 'width', '300');
+      this.renderer.setAttribute(content, 'height', '200');
+      this.renderer.setAttribute(content, 'viewBox', '0 0 300 200');
+      this.renderer.appendChild(this.canvasControl.nativeElement, content);
+    }
+
+    const rectangle = this.renderer.createElement('rect', 'svg');
+
+    this.renderer.setAttribute(rectangle, 'x', this.generateRandomPosition(250).toString());
+    this.renderer.setAttribute(rectangle, 'y', this.generateRandomPosition(150).toString());
+    this.renderer.setAttribute(rectangle, 'width', '100');
+    this.renderer.setAttribute(rectangle, 'height', '100');
+
+    this.renderer.appendChild(content, rectangle);
+
+    this.elementsCounter++;
+  }
+
+  public createStars() {
+    if (this.elementsCounter >= this.maxElements) {
+      return;
+    }
+
+    let content = this.canvasControl.nativeElement.querySelector('svg');
+
+    if (!content) {
+      content = this.renderer.createElement('svg', 'svg');
+      this.renderer.setAttribute(content, 'width', '300');
+      this.renderer.setAttribute(content, 'height', '300');
+      this.renderer.setAttribute(content, 'viewBox', '0 0 300 300');
+      this.renderer.appendChild(this.canvasControl.nativeElement, content);
+    }
+
+    const polygon = this.renderer.createElement('polygon', 'svg');
+
+    this.renderer.setAttribute(polygon, 'points', '150,25 179,111 270,111 196,165 223,251 150,200 77,251 104,165 30,111 121,111');
+    this.renderer.setAttribute(polygon, 'transform', `translate(${this.generateRandomPosition(300 - 75)},${this.generateRandomPosition(300 - 75)}) scale(0.5)`);
+    this.renderer.appendChild(content, polygon);
+
+    this.elementsCounter++;
+  }
+}
